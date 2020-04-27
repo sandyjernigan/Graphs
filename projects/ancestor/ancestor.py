d03a3ddd409285b7bd110203ee4bc535807b8ff9
Sandy <sandy.jernigan@gmail.com>
2020-04-27 03:07:33 -0400

    Ancestor -- tests pass

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -42,7 +42,6 @@ def earliest_ancestor(ancestors, starting_node):
     # print(ancestorsGraph.vertices)
 
     ''' Get The Earliest Ancestor '''
-    parents = []
 
     # Create a plan_to_visit Stack
     plan_to_visit = Stack()
@@ -50,9 +49,13 @@ def earliest_ancestor(ancestors, starting_node):
 
     # create a Set for visited_vertices
     visited_vertices = set()
+
+    # If the input individual has no parents, the function should return -1.
+    parent_node = -1
     
     # while the plan_to_visit stack is not Empty:
     while plan_to_visit.size() > 0:
+        parents = []
 
         # pop the first vertex from the stack
         current_vertex = plan_to_visit.pop()
@@ -75,28 +78,26 @@ def earliest_ancestor(ancestors, starting_node):
                         # Add to list
                         plan_to_visit.push(parent)
                         parents.append(parent)
-
-    print (parents)
-
-    # If no parents found, return with -1
-    if len(parents) == 0:
-        return -1
-
-    # if only 1 parents found, return with the parent
-    if len(parents) == 1:
-        return parents[0]
-
-    # More than 1 parent found, return the one with the lowest numeric ID
-    return min(parents)
+            
+            # Return parent with the lowest numeric ID
+            if len(parents) > 0:
+                parent_node = min(parents)
+                
+    return parent_node
 
 
 # Test Function
 test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
 
 def testFunction(testThis, shouldResult):
-    print (f"Test: {testThis}")
-    print (f"Test result: {earliest_ancestor(test_ancestors, testThis)}")
-    print (f"Should result: {shouldResult}")
+    results = earliest_ancestor(test_ancestors, testThis)
+
+    if results == shouldResult:
+        print (f"Test: {testThis} -- Passed")
+    else:
+        print(f"Test: {testThis} -- Failed")
+        print (f"Test Result: {results}")
+        print (f"Should Result: {shouldResult}")
 
 testThis = 1
 shouldResult = 10
@@ -106,47 +107,38 @@ testThis = 2
 shouldResult = -1
 testFunction(testThis, shouldResult)
 
-# testThis = earliest_ancestor(test_ancestors, 3)
-# testResult = 10
-# print (f"Test result: {testThis}")
-# print (f"Should result: {testResult}")
+testThis = 3
+shouldResult = 10
+testFunction(testThis, shouldResult)
 
-# testThis = earliest_ancestor(test_ancestors, 4)
-# testResult = -1
-# print (f"Test result: {testThis}")
-# print (f"Should result: {testResult}")
+testThis = 4
+shouldResult = -1
+testFunction(testThis, shouldResult)
 
-# testThis = earliest_ancestor(test_ancestors, 5)
-# testResult = 4
-# print (f"Test result: {testThis}")
-# print (f"Should result: {testResult}")
+testThis = 5
+shouldResult = 4
+testFunction(testThis, shouldResult)
 
-# testThis = earliest_ancestor(test_ancestors, 6)
-# testResult = 10
-# print (f"Test result: {testThis}")
-# print (f"Should result: {testResult}")
+testThis = 6
+shouldResult = 10
+testFunction(testThis, shouldResult)
 
-# testThis = earliest_ancestor(test_ancestors, 7)
-# testResult = 4
-# print (f"Test result: {testThis}")
-# print (f"Should result: {testResult}")
+testThis = 7
+shouldResult = 4
+testFunction(testThis, shouldResult)
 
-# testThis = earliest_ancestor(test_ancestors, 8)
-# testResult = 4
-# print (f"Test result: {testThis}")
-# print (f"Should result: {testResult}")
+testThis = 8
+shouldResult = 4
+testFunction(testThis, shouldResult)
 
-# testThis = earliest_ancestor(test_ancestors, 9)
-# testResult = 4
-# print (f"Test result: {testThis}")
-# print (f"Should result: {testResult}")
+testThis = 9
+shouldResult = 4
+testFunction(testThis, shouldResult)
 
-# testThis = earliest_ancestor(test_ancestors, 10)
-# testResult = -1
-# print (f"Test result: {testThis}")
-# print (f"Should result: {testResult}")
+testThis = 10
+shouldResult = -1
+testFunction(testThis, shouldResult)
 
-# testThis = earliest_ancestor(test_ancestors, 11)
-# testResult = -1
-# print (f"Test result: {testThis}")
-# print (f"Should result: {testResult}")
+testThis = 11
+shouldResult = -1
+testFunction(testThis, shouldResult)
